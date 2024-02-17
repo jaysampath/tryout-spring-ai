eaa9ec26da3e4e06bf62b5aeea6e8ed60358fa4d
Jay <50858217+jaysampath@users.noreply.github.com>
2024-02-17 20:32:10 +0530

    Spring AI starter project with OpenAI

---
+++ src/main/java/com/example/spring/ai/explorespringai/ExploreSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.ai.explorespringai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreSpringAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/ai/explorespringai/ExploreSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.ai.explorespringai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
